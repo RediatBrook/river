18f548f91462e9043dbc1ba00ed538336b2e705c
Rediat Shamsu <rediatbrook@gmail.com>
2023-12-26 01:10:25 -0600

    Added basic support for local models (Zephyr)

--- frontend.py
+++ frontend.py
@@ -2,17 +2,43 @@ from openai import OpenAI
 import streamlit as st
 from dotenv import load_dotenv
 import os
+from llama_cpp import Llama
+import time
 
 load_dotenv()
 
+if "prev_model_selected" not in st.session_state:
+    st.session_state["prev_model_selected"] = None
 
 llm_selected = st.selectbox(
    "Model:",
-   ("OpenAI", "Phi 2 (Local)"),
+   ("OpenAI", "Phi", "Zephyr"),
    index=None,
    placeholder="Choose a Model...",
    label_visibility="hidden"
 )
+
+if llm_selected == "Phi" and st.session_state["prev_model_selected"] != "Phi":
+    st.session_state["llm"] = Llama(model_path="./models/phi2.gguf", chat_format="llama-2")
+    progress_text = "Loading Phi 2 model..."
+    local_llm_loadbar = st.progress(0, text=progress_text)
+    for percent_complete in range(100):
+        time.sleep(0.05)
+        local_llm_loadbar.progress(percent_complete + 1, text=progress_text)
+    time.sleep(1)
+    local_llm_loadbar.empty()
+    st.session_state["prev_model_selected"] = "Phi"
+
+if llm_selected == "Zephyr" and st.session_state["prev_model_selected"] != "Zephyr":
+    st.session_state["llm"] = Llama(model_path="./models/zephyr.gguf", chat_format="zephyr", n_ctx=3096)
+    progress_text = "Loading Zephyr model..."
+    local_llm_loadbar = st.progress(0, text=progress_text)
+    for percent_complete in range(100):
+        time.sleep(0.08)
+        local_llm_loadbar.progress(percent_complete + 1, text=progress_text)
+    time.sleep(1)
+    local_llm_loadbar.empty()
+    st.session_state["prev_model_selected"] = "Zephyr"
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
@@ -45,7 +71,21 @@ if prompt := st.chat_input("Enter your message..."):
             ):
                 full_response += (response.choices[0].delta.content or "")
                 message_placeholder.markdown(full_response + "▌")
-        elif llm_selected == "Phi 2 (Local)":
-            st.write("You chose Phi 2!")
+        else:
+            for response in st.session_state["llm"].create_chat_completion(
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True
+            ):
+                # print(response)
+                # st.write(response)
+                if 'content' in response['choices'][0]['delta']:
+                    full_response += (response['choices'][0]['delta']['content'] or "")
+                else:
+                    continue
+                message_placeholder.markdown(full_response + "▌")
+
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
